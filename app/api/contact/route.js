1af4f5ee22ff89263f3b14005e522f52c0297f23
Manav Manish Parmar <mmparmar044@gmail.com>
2025-07-07 16:33:53 +0530

    Complete nodemailer test in public

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -7,7 +7,7 @@ export async function POST(req) {
     service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      pass: process.env.EMAIL_PASSWORD,
     },
   })
 
